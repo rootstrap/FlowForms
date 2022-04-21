e4506aa0e6d3bdcf72cd66ab81f8c66d5b8b1803
Hrodrick <rodrigo.soria@rootstrap.com>
2022-04-21 15:54:58 -0300

    Standarize dependencies constants with snake case

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,49 +1,48 @@
 import org.gradle.api.artifacts.dsl.DependencyHandler
 
 object Dependencies {
+
     // Kotlin
-    private const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN}"
-    private const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES_CORE}"
+    private const val KOTLIN_STD_LIB = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.KOTLIN}"
+    private const val COROUTINES_CORE = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.COROUTINES_CORE}"
 
-    // Android ui
-
-    private const val appcompat = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
-    private const val coreKtx = "androidx.core:core-ktx:${Versions.CORE_KTX}"
-    private const val material = "com.google.android.material:material:${Versions.MATERIAL}"
-    private const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
+    // Android
+    private const val APP_COMPAT = "androidx.appcompat:appcompat:${Versions.APP_COMPAT}"
+    private const val CORE_KTX = "androidx.core:core-ktx:${Versions.CORE_KTX}"
+    private const val MATERIAL = "com.google.android.material:material:${Versions.MATERIAL}"
+    private const val CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:${Versions.CONSTRAINT_LAYOUT}"
 
     // Test libs
-
-    private const val junit = "junit:junit:${Versions.J_UNIT}"
-    private const val extJUnit = "androidx.test.ext:junit:${Versions.EXT_J_UNIT}"
-    private const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
+    private const val J_UNIT = "junit:junit:${Versions.J_UNIT}"
+    private const val EXT_J_UNIT = "androidx.test.ext:junit:${Versions.EXT_J_UNIT}"
+    private const val ESPRESSO_CORE = "androidx.test.espresso:espresso-core:${Versions.ESPRESSO}"
 
     val kotlinLibraries = arrayListOf<String>().apply {
-        add(kotlinStdLib)
-        add(coroutinesCore)
+        add(KOTLIN_STD_LIB)
+        add(COROUTINES_CORE)
     }
 
     val flowFormsAndroidLibraries = arrayListOf<String>().apply {
-        add(coreKtx)
-        add(appcompat)
-        add(material)
+        add(CORE_KTX)
+        add(APP_COMPAT)
+        add(MATERIAL)
     }
 
     val appLibraries = arrayListOf<String>().apply {
-        add(kotlinStdLib)
-        add(coreKtx)
-        add(appcompat)
-        add(material)
-        add(constraintLayout)
+        add(KOTLIN_STD_LIB)
+        add(CORE_KTX)
+        add(APP_COMPAT)
+        add(MATERIAL)
+        add(CONSTRAINT_LAYOUT)
     }
 
     val androidTestLibraries = arrayListOf<String>().apply {
-        add(extJUnit)
-        add(espressoCore)
+        add(EXT_J_UNIT)
+        add(ESPRESSO_CORE)
     }
 
     val testLibraries = arrayListOf<String>().apply {
-        add(junit)
+        add(J_UNIT)
     }
 }
 
