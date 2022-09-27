7592c348de925616e82749771a0dfc566fcb7da6
Hrodrick <rodrigo.soria@rootstrap.com>
2022-09-27 12:05:44 -0300

    Animated chevron and quickstart section

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -3,3 +3,9 @@ $("#menu-toggle").click(function(e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
 });
+
+$("#quickstart-guides").click(function(e) {
+    e.preventDefault();
+    $("#quickstart-guides-items").toggleClass("active");
+    $("#quickstart-guides-chevron").toggleClass("chevron-active");
+});
