06d5b1539a379d5624660799e68cd44bd3cd55f4
Ximena Perez <ximena@rootstrap.com>
2023-01-12 12:19:06 -0300

    Remove comments

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,14 +23,6 @@ allprojects {
 }
 
 tasks {
-   /* register<Copy>("copyGitHooks") {
-        description = "Copies the git hooks from root to the .git folder."
-        from("$rootDir") {*/
-           // include("**/*.sh")
-         //   rename("(.*).sh", "$1")
-     //   }
-      //  into("$rootDir/.git/hooks")
-  //  }
     register<Copy>("copyGitHooks") {
         description = "Copies the git hooks from root to the .git folder."
         from("$rootDir") {
@@ -51,42 +43,10 @@ tasks {
             delete(preCommit)
         }
     }
-
-   /* register<Exec>("installGitHooks") {
-        description = "Installs the pre-commit git hooks from root."
-      //  group = BuildTaskGroups.GIT_HOOKS
-        workingDir(rootDir)
-        commandLine("chmod")
-        args("-R", "+x", ".git/hooks/")
-        dependsOn(named("copyGitHooks"))
-      /*  onlyIf {
-            isLinuxOrMacOs()
-        }*/
-        doLast {
-            logger.info("Git hooks installed successfully.")
-        }
-    }*/
-
-  /*  register<Delete>("deleteGitHooks") {
-        description = "Delete the pre-commit git hooks."
-     //   group = BuildTaskGroups.GIT_HOOKS
-        delete(fileTree(".git/hooks/"))
-    }*/
-
-   /* afterEvaluate {
-        tasks["clean"].dependsOn(tasks.named("copyGitHooks"))
-    }*/
 }
-
-/*task installGitHook(type: Copy) {
-    from new File(rootProject.rootDir, 'pre-commit')
-    into { new File(rootProject.rootDir, '.git/hooks') }
-    fileMode 0777
-}*/
 
 project.tasks.getByPath("copyGitHooks").dependsOn(tasks.named("deletePreviousGitHooks"))
 project.tasks.getByPath(":FlowForms-Core:preBuild").dependsOn(tasks.named("copyGitHooks"))
-//tasks[":app:preBuild"].dependsOn(tasks.named("copyGitHooks"))
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
