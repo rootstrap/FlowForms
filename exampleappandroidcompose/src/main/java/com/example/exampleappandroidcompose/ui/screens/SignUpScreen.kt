51870fbbbed11ca625ee386a5a5c1ee38f5301b9
Hrodrick <rodrigo.soria@rootstrap.com>
2023-09-04 10:53:16 -0400

    Fix viewmodel causing recompositions

--- exampleappandroidcompose/src/main/java/com/example/exampleappandroidcompose/ui/screens/SignUpScreen.kt
+++ exampleappandroidcompose/src/main/java/com/example/exampleappandroidcompose/ui/screens/SignUpScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -21,7 +22,7 @@ import com.rootstrap.flowforms.shared.SignupViewModel
 
 @Composable
 fun SignUpScreen(
-    viewModel: SignupViewModel = SignupViewModel()
+    viewModel: SignupViewModel = remember { SignupViewModel() }
 ) {
     val uiState by viewModel.uiState.collectAsState()
     SignUpScreen(
